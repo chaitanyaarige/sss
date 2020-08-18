97e3f552dc4c430103231be45120773efb907a76
chaitanya <ckrishna@jazeeratech.in>
2020-08-18 18:55:54 +0530

    added publisher page

--- src/App.vue
+++ src/App.vue
@@ -20,11 +20,12 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "ubuntu-regular", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   max-width: 1200px;
+  font-size: 15px;
   margin: auto;
   background: lightcyan;
 }
--- src/views/publishers.vue
+++ src/views/publishers.vue
@@ -1,31 +1,15 @@
 <template>
   <div>
-    <b-container class="bv-example-row">
-      <b-row>
-        <b-col cols="2">
-          <div class="text-center">
-            <b-button variant="primary">
-              Add New
-              <b-badge variant="light">4</b-badge>
-            </b-button>
-          </div>
-        </b-col>
-      </b-row>
-    </b-container>
+    <div class='ml-2' style="display:flex">
+      <div class="m-4 Publisher__underline-active">Add Books</div>
+      <div class="m-4 Publisher__underline">Add Govt Books</div>
+      <div class="m-4 Publisher__underline">Add Stationary</div>
+    </div>
 
-    <b-container class="mt-4">
-      <b-row>
-        <b-col>
-          <b-row>
-            <b-col cols="5">
-              <b-table striped hover :items="items" :fields="fields"></b-table>
-            </b-col>
-            <b-col cols="2">
-              <b-button variant="primary">Save</b-button>
-            </b-col>
-          </b-row>
-        </b-col>
-      </b-row>
+    <b-container>
+      <b-card>
+        <b-table hover :items="items" :fields="fields"></b-table>
+      </b-card>
     </b-container>
   </div>
 </template>
@@ -33,30 +17,98 @@
 
 <script>
 export default {
-     data() {
-      return {
-        fields: [
-          {
-            key: 'last_name',
-            sortable: true
-          },
-          {
-            key: 'first_name',
-            sortable: false
-          },
-          {
-            key: 'age',
-            label: 'Person age',
-            sortable: true,
-          }
-        ],
-        items: [
-          { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-          { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-          { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-          { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
-        ]
+  data() {
+    return {
+      fields: [
+        {
+          key: "last_name",
+          sortable: true,
+        },
+        {
+          key: "first_name",
+          sortable: false,
+        },
+        {
+          key: "age",
+          label: "Person age",
+          sortable: true,
+        },
+        {
+          key: "last_names",
+          label: "Person age",
+          sortable: false,
+        },
+      ],
+      items: [
+        {
+          isActive: true,
+          age: 20,
+          first_name: "Dickerson",
+          last_name: "Macdonald",
+          last_names: "Wilson",
+        },
+        {
+          isActive: true,
+          age: 40,
+          first_name: "Dickerson",
+          last_name: "Macdonald",
+          last_names: "Wilson",
+        },
+        {
+          isActive: true,
+          age: 40,
+          first_name: "Dickerson",
+          last_name: "Macdonald",
+          last_names: "Wilson",
+        },
+        {
+          isActive: true,
+          age: 40,
+          first_name: "Dickerson",
+          last_name: "Macdonald",
+          last_names: "Wilson",
+        },
+        {
+          isActive: true,
+          age: 40,
+          first_name: "Dickerson",
+          last_name: "Macdonald",
+          last_names: "Wilson",
+        },
+        {
+          isActive: true,
+          age: 40,
+          first_name: "Dickerson",
+          last_name: "Macdonald",
+          last_names: "Wilson",
+        },
+        {
+          isActive: true,
+          age: 40,
+          first_name: "Dickerson",
+          last_name: "Macdonald",
+          last_names: "Wilson",
+        },
+        {
+          isActive: true,
+          age: 40,
+          first_name: "Dickerson",
+          last_name: "Macdonald",
+          last_names: "Wilson",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .Publisher {
+    &__underline {
+      cursor:pointer;
+      &-active {
+        border-bottom: 4px solid darkcyan;
       }
     }
   }
-</script>
+</style>
