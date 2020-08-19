3e631145eddd09ce74f015f6046348f602b564b1
chaitanya <ckrishna@jazeeratech.in>
2020-08-19 12:59:21 +0530

    tset

--- src/components/temp.vue
+++
@@ -1,7 +0,0 @@
-              <b-list-group-item href="#">Stock Return</b-list-group-item>
-              <b-list-group-item href="#">Buyer List</b-list-group-item>
-              <b-list-group-item href="#">sub Schools List</b-list-group-item>
-              <b-list-group-item href="#">sub Companies List</b-list-group-item>
-              <b-list-group-item to='publishers'>Publishers List</b-list-group-item>
-              <b-list-group-item href="#">Audit</b-list-group-item>
-    </nav>-->
