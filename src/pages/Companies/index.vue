0b1481ffc24097aa50a66b6a8e2611eae13c0226
chaitanya <ckrishna@jazeeratech.in>
2020-08-21 22:54:38 +0530

    companies

--- src/pages/Companies/index.vue
+++ src/pages/Companies/index.vue
@@ -85,29 +85,9 @@
     </div>
 
     <div class="Schools__second-container">
-      <div class="Schools__main-title">Edit Company Data</div>
-      <div class="Schools__toggle-form">
-        <svg
-          width="25px"
-          height="25px"
-          viewBox="0 0 16 16"
-          class="bi bi-pencil"
-          :fill="leftColor"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M11.293 1.293a1 1 0 0 1 1.414 0l2 2a1 1 0 0 1 0 1.414l-9 9a1 1 0 0 1-.39.242l-3 1a1 1 0 0 1-1.266-1.265l1-3a1 1 0 0 1 .242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z"
-          />
-          <path
-            fill-rule="evenodd"
-            d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 0 0 .5.5H4v.5a.5.5 0 0 0 .5.5H5v.5a.5.5 0 0 0 .5.5H6v-1.5a.5.5 0 0 0-.5-.5H5v-.5a.5.5 0 0 0-.5-.5H3z"
-          />
-        </svg>
-      </div>
     </div>
 
-    <div class="Schools__sub-container-two">
+    <!-- <div class="Schools__sub-container-two">
       <div class="Schools__first-row">
         <multiselect
           v-model="newCompany.name"
@@ -117,7 +97,7 @@
           label="name"
         ></multiselect>
       </div>
-    </div>
+    </div> -->
 
     <div class="Schools__third-container">
       <div class="Schools__main-title">Company Data</div>
