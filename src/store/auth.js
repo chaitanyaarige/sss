684cbe216d0589dd80c3982ec435165873e3dc57
chaitanya <ckrishna@jazeeratech.in>
2020-08-26 12:49:12 +0530

    localstorage

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,20 +1,35 @@
 export default {
   namespaced: true,
   state: {
-    isLoggedIn: false
+    isLoggedIn: true,
+    user: []
   },
   getters: {
-    isLoggedIn: state => state.isLoggedIn
+    isLoggedIn: state => state.isLoggedIn,
+    user: state => state.user
   },
   actions: {},
   mutations: {
     newLogin(state, data) {
       if (data.email === "sonu@sss.com" && data.password === "sonu") {
         state.isLoggedIn = true;
+        this.assignLocalStorage()
       }
     },
+    assignLocalStorage() {
+      localStorage.setItem('isLoggedIn', JSON.stringify(state.isLoggedIn))
+      localStorage.setItem('userDetails', JSON.stringify(data))
+    },
+    getLocalStorage(state) {
+      state.user = JSON.parse(localStorage.getItem('isLoggedIn'))
+    },
+    clearLocalStorage() {
+      localStorage.removeItem('isLoggedIn')
+      localStorage.removeItem('userDetails')
+    },
     logoutCustomer(state, data) {
-      state.isLoggedIn = false;
+      state.isLoggedIn = false
+      this.clearLocalStorage()
     }
   }
 };
